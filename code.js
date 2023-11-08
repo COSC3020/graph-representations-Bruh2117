55531f603a970bd31aa68dd800866ec294f4107d
Aidan Giraldo <agirald1@uwyo.edu>
2023-11-07 18:12:49 -0700

    code.js and README complete

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    var adjList = []; 
+
+    for (var i = 0; i < adjMatrix.length; i++)
+    {
+        var row = adjMatrix[i]; 
+        var tmp = []; 
+        var j = 0; 
+        
+        while (j < row.length)
+        {
+            if (row[j] == 1)
+            {
+                tmp.push(j); 
+            }
+            j++; 
+        }
+        adjList[i] = tmp; 
+    }
+    return adjList;
 }
